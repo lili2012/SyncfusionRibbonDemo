77f8c92e58beef88a9c75ae4565f53c4889972bc
Li Shulian <lishulian@example.com>
2024-09-17 21:46:41 +0800

    shadow dom

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import App from './App.vue'
 import { createApp } from 'vue'
 import { defineCustomElement } from 'vue'
+
+//https://github.com/vuejs/core/pull/11517
+//https://github.com/ElMassimo/vue-custom-element-example
+//https://github.com/vuejs/core/issues/4662
 import HelloWorld from './components/HelloWorld.ce.vue'
 customElements.define('hello-world', defineCustomElement(HelloWorld));
 
 import { registerLicense } from '@syncfusion/ej2-base';
+
 
 registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhIfkx/WmFZfVpgdVRMY15bQHdPIiBoS35RckVqWH5ec3RRQ2RYWEB1');
 createApp(App).mount('#app')

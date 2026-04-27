68ed72e5c31581d4fba43ce50c7c26fd1bb523de
LiShulian <13984242+shulian88_admin@user.noreply.gitee.com>
2026-04-27 22:46:38 +0800

    commit

--- src/components/App.vue
+++ src/components/App.vue
@@ -82,7 +82,11 @@
     overflowMode='Scrollable' headerPlacement="Top" cssClass="e-fill" :selected='selected' :removing='removing'
     :showCloseButton=true>
     <e-tabitems>
-      <e-tabitem :header="headerText0" :content="content0"></e-tabitem>
+      <e-tabitem :header="headerText0"  :content="'introPageComponent'"></e-tabitem>
+       <template v-slot:introPageComponent>
+          <IntroPage id="calendar"></IntroPage>
+          <div id="heading">Sign in to view the Grid</div>
+        </template>
       <e-tabitem :header="headerText1" :content="content1"></e-tabitem>
       <e-tabitem :header="headerText2" :content="content2"></e-tabitem>
       <e-tabitem :header='addTabHeader' cssClass="withoutIcon"></e-tabitem>
@@ -98,7 +102,7 @@ import { TabComponent as EjsTab, TabItemsDirective as ETabitems, TabItemDirectiv
 import { CommandStack } from "sgcad";
 import Drawing from "@/components/Drawing.vue"
 import { useFileDialog } from '@vueuse/core'
-
+import IntroPage from "@/components/IntroPage.vue"
 
 const TabInstance = useTemplateRef('TabInstance')
 provide('ribbon', [RibbonFileMenu, RibbonColorPicker]);
@@ -267,7 +271,7 @@ const headerText0 = { text: "工程介绍" };
 const headerText1 = { text: "项目进展" };
 const headerText2 = { text: "施工管理" };
 const addTabHeader = { 'iconCss': 'e-plus' }
-const content0 = `肥西核电站平面设计图是该核电站整体规划与布局的重要表现形式，涵盖了核电站内各主要建筑物、设施以及设备的空间位置安排。该设计图主要呈现了核电站的结构布局，包括反应堆厂房、蒸汽发生器、涡轮发电机组、电力变电站、冷却塔等关键组成部分的相对位置。核电站的平面设计图通常依据核安全标准和环境影响评估要求，精确标明了各设施的布置，以确保运行时的安全性、效率性和环境适应性。例如，反应堆厂房通常设有多重安全隔离层，以防止核泄漏，蒸汽发生器与涡轮机组的配置则优化了热能转换过程。此外，冷却系统的设计确保了反应堆在高温下稳定运行，并通过冷却塔实现热量的释放。在设计过程中，还需要考虑到电力输送的线路和区域规划，以确保核电站的输出能够有效连接到国家或地区的电网系统，满足不同时间段的电力需求。`;
+
 const content1 = `肥西核电站的立面设计图主要展示了核电站各建筑物外立面的视觉效果与结构形态，旨在兼顾安全性、功能性和美观性。立面设计不仅体现了建筑的外观，还涉及到结构的抗震、防辐射等要求，以保证核电站在极端环境下的稳定性与安全性。在肥西核电站的立面设计中，反应堆厂房通常具有坚固的外墙结构，以抵御外部压力和辐射，外立面材料往往采用高强度、防辐射的混凝土或钢材，并可能配备防护屏障。此外，涡轮发电机组、冷却塔等大型设施也在立面设计中有独立的表现，通常呈现出高耸、简洁的几何形态，以适应其高效运行需求。立面设计还需要考虑到设施与周边环境的和谐融入。例如，冷却塔的高大圆形设计不仅满足冷却需求，还在视觉上具有独特的标志性，使核电站在外观上易于识别，同时确保不对周围生态环境造成过大影响。此外，立面图中还会标出建筑的入口、窗户、通风口等细节位置，以确保功能区域的合理分布和人员流动的顺畅。设计师在考虑核电站安全标准的同时，也会注重建筑的美学效果，使其在保障安全与技术要求的基础上，呈现出简洁现代的外观。`;
 const content2 = `肥西核电站立面详图是对核电站建筑外立面细部设计的详细呈现，通常包括了建筑外观、结构构件、装饰元素及细节处理等内容。这类设计图不仅提供了建筑物外立面的精确尺寸、比例、材质，还展示了各部分与其他建筑构件的连接方式，确保了核电站在外观上的安全性、功能性与美观性。在肥西核电站的立面详图中，反应堆厂房的外墙可能会标出防护层的厚度、材料种类以及施工工艺，例如采用辐射防护混凝土和钢筋混凝土层的具体做法。涡轮发电机组、泵房等设施的立面详细图则涉及到外部通道、窗户、排气口等部件的尺寸和位置安排，确保建筑物的通风、散热及维护管理功能。冷却塔的立面详图通常会标明塔体的高度、直径以及内部结构的承载设计，还可能包括防风、防震等特殊设计要求。此部分详图对于冷却塔的稳定性和安全性至关重要，尤其是在面对强风和极端天气时，冷却塔的结构必须经受住巨大的外部压力。此外，立面详图还会标注建筑表面装饰材料的细节，例如抗风化的外墙涂层或金属外装饰，确保其长期的耐久性与良好的视觉效果。在美学方面，设计师还会在详图中精确呈现建筑物的比例、线条和层次感，以使建筑在严格的功能性要求下，依然能够呈现出简洁、现代的外观。`;
 </script>
